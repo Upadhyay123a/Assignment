36f61586c1f3364d8a1e82fe541af29881af6bae
Atul Kumar Upadhyay <137402498+Upadhyay123a@users.noreply.github.com>
2025-09-21 23:34:45 +0530

    Update CacheLimits.java

--- refactoring-java-Upadhyay123a/src/main/java/com/speechify/cache/CacheLimits.java
+++ refactoring-java-Upadhyay123a/src/main/java/com/speechify/cache/CacheLimits.java
@@ -1,6 +1,5 @@
 package com.speechify.cache;
 
-
 public class CacheLimits {
     public static final int USER_CACHE_SIZE = 100;
     public static final int CLIENT_CACHE_SIZE = 50;
@@ -15,6 +14,7 @@ public class CacheLimits {
         return maxItemsCount;
     }
 }
+
 
 
 
